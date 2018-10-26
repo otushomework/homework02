78a4c3a492299316c05bdbd15c1961bd98ce1d7f
Vitaly Petrov <v31337@gmail.com>
2018-10-26 20:23:57 +0300

    Run travis

--- main.cpp
+++ main.cpp
@@ -31,6 +31,7 @@ std::vector<std::string> split(const std::string &str, char d)
 
 int main(int argc, char const *argv[])
 {
+
     try
     {
         std::vector<std::vector<std::string>> ip_pool;
